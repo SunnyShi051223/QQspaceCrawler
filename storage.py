b3c69f11d1a9d66e2553c1950a586dec34ca3caf
32874 <3287424602@qq.com>
2025-07-28 16:53:46 +0800

    1

--- storage.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-def save_content(qq, contents):
-    os.makedirs('data', exist_ok=True)
-    file_path = os.path.join('data', f'{qq}_shuoshuo.json')
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(contents, f, ensure_ascii=False, indent=2)
